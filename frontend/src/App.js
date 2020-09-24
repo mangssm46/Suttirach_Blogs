c32051efdcb1dd1bfcabbd2c9591f10eab9cd0e6
mangssm46 <mangssm46@gmail.com>
2020-09-24 11:28:30 +0700

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Route } from 'react-router-dom'
+import './App.css';
+import { Container } from 'reactstrap';
+import Header from './components/Header';
+import About from './components/About';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Products from './components/Products';
+import Profile from './components/Profile';
+import Users from './components/Users';
+import Login from './components/Login';
+import Register from './components/Register';
+import category from './components/Category';
+import FileUpload from './components/FileUpload';
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Container fluid={true}>
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/about"   component={About} />
+        <Route path="/products"   component={Products} />
+        <Route path="/profile"   component={Profile} />
+        <Route path="/users"   component={Users} />
+        <Route path="/login"   component={Login} />
+        <Route path="/register"  component={Register} />
+        <Route path="/category"  component={category} />
+        <Route path="/fileupload"  component={FileUpload} />
+      </Container>
+    </div>
+  
+  );
+}
+
+export default App;
